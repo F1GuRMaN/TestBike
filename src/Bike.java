008f1ebc3f8399e47d560a45323031511302bbd1
Hurynovich Dzmitry <F182GD@gmail.com>
2014-03-10 23:08:41 +0200

    added java files: Bike and TestBike

---
+++ src/Bike.java
@@ -0,0 +1,10 @@
+public class Bike {
+	public String color;
+	 public int numberOfWheel;
+
+	void statrBike() {
+			}
+	void stopBike() {
+	}
+	}
+	
---
+++ src/TestBike.java
@@ -0,0 +1,14 @@
+
+
+public class TestBike {
+
+	public static void main(String[] args) {
+		
+		Bike bike1 = new Bike();
+		 bike1.color = "red";
+		 bike1.numberOfWheel = 3;
+		 System.out.println(bike1.color + " " + bike1.numberOfWheel);
+
+	}
+
+}
